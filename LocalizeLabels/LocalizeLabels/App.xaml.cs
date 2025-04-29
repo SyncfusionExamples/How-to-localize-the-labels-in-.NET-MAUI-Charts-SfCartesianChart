f58a8a594ebd093fe6be33c80fa361f9c05ba444
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-29 13:17:21 +0530

    Resx file added

--- LocalizeLabels/LocalizeLabels/App.xaml.cs
+++ LocalizeLabels/LocalizeLabels/App.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace LocalizeLabels
+﻿using Syncfusion.Maui.Charts;
+using System.Globalization;
+using System.Resources;
+
+namespace LocalizeLabels
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            CultureInfo.CurrentUICulture = new CultureInfo("it-IT");
+            var ResXPath = "LocalizeLabels.Resources.SfCartesianChart";
+            SfCartesianChartResources.ResourceManager = new ResourceManager(ResXPath, Application.Current.GetType().Assembly);
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
